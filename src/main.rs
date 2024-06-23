33a8e971fb647c102ca074aceaba34c1cf402034
rekky1aws <killikomfr@gmail.com>
2024-06-23 23:11:23 +0200

    First working version
    NOT BUG PROOF
    Source value only accepting numbers in base below and equal to 10.
    
    Functions :
     - highest_weight_value : Find the power value of the highest weight
       digit in a given base.
     - dec_to_b : Transform a decimal number into a given base.

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,31 @@ fn simple_print (text: &str) {
     stdout().flush().unwrap();
 }
 
+fn highest_weight_value (n: u32, b: u8) -> u32 {
+    let base: u32 = b.into();
+
+	let mut weight_power = 0;
+
+	while base.pow(weight_power) < n {
+		weight_power += 1;
+	}
+
+	return weight_power - 1;
+}
+
 fn b_to_dec (n: String, b: u8) -> u32 {
     let base: u32 = b.into();
 
     let mut result = 0;
     let mut counter = 0;
 
+    // Splitting n
     let mut parts: Vec<_> = n.split("").collect::<Vec<_>>();
     parts.retain(|&x| x != "");
 
     for i in (0..parts.len()).rev() {
         let element = parts[i];
 
-        println!("{:?}", element.parse::<u32>().unwrap());
         result += base.pow(counter) * element.parse::<u32>().unwrap();
 
         counter += 1;
@@ -45,9 +57,23 @@ fn b_to_dec (n: String, b: u8) -> u32 {
     return result.try_into().unwrap();
 }
 
-/*fn dec_to_b (n: String, b: u8) {
+fn dec_to_b (n: u32, b: u8) -> String {
+	let base: u32 = b.into();
+	let hwv = highest_weight_value(n, b);
 
-}*/
+	let mut n_remaining = n.clone();
+	let mut result = String::new();
+
+	for i in (0..hwv+1).rev() {
+		let val = n_remaining / (base.pow(i));
+		if val > 0 {
+			n_remaining = n_remaining % (base.pow(i) * val);
+		}
+		result = format!("{}{}", result, val);
+	}
+
+	return result;
+}
 
 fn main() {
     stdout().flush().unwrap();
@@ -55,12 +81,15 @@ fn main() {
     simple_print("Source base [2, 255]:\t");
     let in_base = read_u8();
 
-    simple_print("Source value :\t\t");
+    simple_print("Source value [1, ∞[:\t");
     let in_value = read_string();
 
     simple_print("Ouput base [2, 255]:\t");
-    let _out_base = read_u8();
+    let out_base = read_u8();
 
-    let number_dec = b_to_dec(in_value, in_base);
-    println!("{:?}", number_dec);
+    let number_dec = b_to_dec(in_value.clone(), in_base);
+    let out_value = dec_to_b(number_dec, out_base);
+
+    println!("{:?} in base({:?}) is {:?} in base({:?})", in_value, in_base, out_base, out_value);
+    println!("\t>{:?}", out_value);
 }
